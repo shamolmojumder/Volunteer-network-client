9602b96532a67ea0cf38fd0ad48666c31485f411
shamolmojumder <shamolmojumder14@gmail.com>
2021-10-16 00:25:34 +0600

    48_5-7 Upload events with image link to database

--- src/components/AddEvents/AddEvents.js
+++ src/components/AddEvents/AddEvents.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const AddEvents = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const [imageURL,setIMageURL]=useState(null);
+
+  const onSubmit = data =>{
+    const eventData={
+      name:data.name,
+      imageURL:imageURL
+    }
+    const url=`http://localhost:5055/addEvent`;
+    console.log(eventData);
+    fetch(url,{
+      method:'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body:JSON.stringify(eventData)
+    })
+    .then(res=>console.log("server side response",res))
+  }
+  
   const handleImageUpload=event=>{
     console.log(event.target.files[0]);
     const imageData=new FormData();
@@ -14,7 +32,7 @@ const AddEvents = () => {
     axios.post('https://api.imgbb.com/1/upload',
     imageData)
     .then(function (response) {
-      console.log(response.data.data.display_url);
+      setIMageURL(response.data.data.display_url);
     })
     .catch(function (error) {
       console.log(error);
@@ -24,7 +42,7 @@ const AddEvents = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-      <input defaultValue="shamol" {...register("example")} />
+      <input name="name" defaultValue="New Exciting Event" ref={register} />
       <br />
       <input type="file" onChange={handleImageUpload} />
       <br />
